2857eb98e9b223adea61a1ae600651464e75dcb5
Alexey Suslov <suslov.aleksey.oo7@gmail.com>
2022-08-06 20:15:37 +0300

    Preparing to delete pods

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -11,11 +11,10 @@ import CLTypingLabel
 
 class WelcomeViewController: UIViewController {
 
-    @IBOutlet weak var titleLabel: CLTypingLabel!
+    @IBOutlet weak var titleLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        titleLabel.text = "⚡️FlashChat"
     }
 }
